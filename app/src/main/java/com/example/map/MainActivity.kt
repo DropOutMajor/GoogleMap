4dc4e342d1885782605879ab89ee9885d6cedade
DropOut <anzaramavhoy@gmail.com>
2024-08-12 19:58:03 +0200

    marker added

--- app/src/main/java/com/example/map/MainActivity.kt
+++ app/src/main/java/com/example/map/MainActivity.kt
@@ -2,23 +2,31 @@ package com.example.map
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
 class MainActivity : AppCompatActivity(), OnMapReadyCallback {
-    private var mGoogleMap:GoogleMap? = null
+
+    private var mGoogleMap: GoogleMap? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val mapFragment = supportFragmentManager.findFragmentById(R.id.mapFragment) as SupportMapFragment
         mapFragment.getMapAsync(this)
-
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
-        TODO("Not yet implemented")
-        mGoogleMap=googleMap
+        mGoogleMap = googleMap
+
+        // Add a marker at a specific location (e.g., Sydney, Australia) and move the camera
+        val sydney = LatLng(-34.0, 151.0)
+        mGoogleMap?.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+        mGoogleMap?.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 10f))
     }
 }
