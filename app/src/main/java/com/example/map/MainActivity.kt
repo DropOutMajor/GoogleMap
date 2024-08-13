017be09af474da5f289738ae97f90357e1f406ba
DropOut <anzaramavhoy@gmail.com>
2024-08-13 21:53:13 +0200

    Map search option

--- app/src/main/java/com/example/map/MainActivity.kt
+++ app/src/main/java/com/example/map/MainActivity.kt
@@ -3,25 +3,53 @@ package com.example.map
 import android.os.Bundle
 import android.widget.ImageButton
 import android.widget.PopupMenu
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.common.api.Status
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.libraries.places.api.Places
+import com.google.android.libraries.places.api.model.Place
+import com.google.android.libraries.places.widget.AutocompleteSupportFragment
+import com.google.android.libraries.places.widget.listener.PlaceSelectionListener
 
 class MainActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private var mGoogleMap: GoogleMap? = null
+    private lateinit var searchbar: AutocompleteSupportFragment
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // Initialize Places API
+        Places.initialize(applicationContext, getString(R.string.google_map_api_key))
+
+        // Setup the autocomplete search bar
+        searchbar = supportFragmentManager.findFragmentById(R.id.search_bar) as AutocompleteSupportFragment
+        searchbar.setPlaceFields(listOf(Place.Field.ID, Place.Field.ADDRESS, Place.Field.LAT_LNG))
+        searchbar.setOnPlaceSelectedListener(object : PlaceSelectionListener {
+            override fun onError(status: Status) {
+                Toast.makeText(this@MainActivity, "Error occurred: ${status.statusMessage}", Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onPlaceSelected(place: Place) {
+                val latLng = place.latLng!!
+                zoomMap(latLng)
+            }
+        })
+
+        // Setup the map fragment
         val mapFragment = supportFragmentManager.findFragmentById(R.id.mapFragment) as SupportMapFragment
         mapFragment.getMapAsync(this)
 
+        // Setup the map options button
         val mapOptionButton: ImageButton = findViewById(R.id.mapMenu)
         val popUpMenu = PopupMenu(this, mapOptionButton)
-        popUpMenu.menuInflater.inflate(R.menu.map_options, popUpMenu.menu) // Fixed reference
+        popUpMenu.menuInflater.inflate(R.menu.map_options, popUpMenu.menu)
 
         popUpMenu.setOnMenuItemClickListener { menuItem ->
             changeMap(menuItem.itemId)
@@ -31,6 +59,11 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
         mapOptionButton.setOnClickListener {
             popUpMenu.show()
         }
+    }
+
+    private fun zoomMap(latLng: LatLng) {
+        val newLatLngZoom = CameraUpdateFactory.newLatLngZoom(latLng, 18f)
+        mGoogleMap?.animateCamera(newLatLngZoom)
     }
 
     private fun changeMap(itemId: Int) {
